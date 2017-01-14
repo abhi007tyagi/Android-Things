a22581cce62f206f5b69dbac4861b7d0f4f940fc
abhinavtyagi <abhi007tyagi@yahoo.co.in>
2017-01-14 21:48:57 +0530

    Speech To Text

---
+++ Voice_Commands/README.md
@@ -0,0 +1,4 @@
+Android Things empty project template
+=====================================
+
+Testing to use Speech to Text API from Android on Android Things. But it's not supported right now.
---
+++ Voice_Commands/app/src/main/java/com/example/androidthings/myproject/MainActivity.java
@@ -0,0 +1,115 @@
+/*
+ * Copyright 2016, The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.androidthings.myproject;
+
+import android.app.Activity;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.os.Bundle;
+import android.speech.RecognizerIntent;
+import android.util.Log;
+
+import com.google.android.things.pio.Gpio;
+import com.google.android.things.pio.GpioCallback;
+import com.google.android.things.pio.PeripheralManagerService;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Locale;
+
+public class MainActivity extends Activity {
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private static final int SPEECH_INPUT = 27;
+    public static final String BTN_PIN = "BCM17"; //physical pin #11
+
+    private Gpio mBtnGpio;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate");
+
+        PeripheralManagerService service = new PeripheralManagerService();
+        try {
+            // Create GPIO connection for PUSH btn
+            mBtnGpio = service.openGpio(BTN_PIN);
+            // Configure as an input.
+            mBtnGpio.setDirection(Gpio.DIRECTION_IN);
+            // Enable edge trigger events for both rising and falling edges.
+            mBtnGpio.setEdgeTriggerType(Gpio.EDGE_BOTH);
+            // Register an event callback.
+            mBtnGpio.registerGpioCallback(mMotorCallback);
+        } catch (IOException e) {
+            Log.e(TAG, "Error on PeripheralIO API", e);
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy");
+        // Close the resource
+        if (mBtnGpio != null) {
+            mBtnGpio.unregisterGpioCallback(mMotorCallback);
+            try {
+                mBtnGpio.close();
+            } catch (IOException e) {
+                Log.e(TAG, "Error on PeripheralIO API", e);
+            }
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+            case SPEECH_INPUT: {
+                if (resultCode == Activity.RESULT_OK && data != null) {
+                    ArrayList<String> result = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                    Log.d(TAG, "onActivityResult: "+result.get(0));
+                }
+                break;
+            }
+        }
+    }
+
+    // Register an event callback.
+    private GpioCallback mMotorCallback = new GpioCallback() {
+        @Override
+        public boolean onGpioEdge(Gpio gpio) {
+            Log.i(TAG, "GPIO callback ------------");
+
+            listen();
+
+            // Return true to keep callback active.
+            return true;
+        }
+    };
+
+    private void listen(){
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        try {
+            Log.d(TAG, "Listening... ");
+            startActivityForResult(intent, SPEECH_INPUT);
+        } catch (ActivityNotFoundException a) {
+            Log.d(TAG, "Sorry! Device does not support speech input");
+        }
+    }
+}
